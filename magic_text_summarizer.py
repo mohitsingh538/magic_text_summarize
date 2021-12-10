fa8f59c0c7d84df0f35b420a8b71185242d4a80b
Mohit Singh <68317657+mohitsingh538@users.noreply.github.com>
2021-12-10 11:58:42 +0530

    Update magic_text_summarizer.py

--- magic_text_summarizer.py
+++ magic_text_summarizer.py
@@ -1,25 +1,38 @@
 from summarizer import Summarizer, TransformerSummarizer
 
-class Summarize_Paragraph():
 
-    def this_paragraph(model_name, paragraph): 
-    #   GPT-2
-        if model_name == "GPT-2":   
-            GPT2_model = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
-            full = ''.join(GPT2_model(paragraph, min_length=5))
-            return full
+class Summarize_Paragraph:
 
-    #   BERT
-        elif model_name == 'BERT':
-            bert_model = Summarizer()
-            bert_summary = ''.join(bert_model(paragraph, min_length=10))
-            return bert_summary
+    def __init__(self, model_name: str):
+        self.model_name = model_name
 
+    def this_paragraph(self, paragraph: str) -> str:
+        #   GPT-2
+        if self.model_name == "GPT-2":
+            try:
+                GPT2_model = TransformerSummarizer(transformer_type="GPT2", transformer_model_key="gpt2-medium")
+                full = ''.join(GPT2_model(paragraph, min_length=5))
+                return full
 
-    #   XLNet
-        elif model_name == 'XLNet':
-            model = TransformerSummarizer(transformer_type="XLNet", transformer_model_key="xlnet-base-cased")
-            full = ''.join(model(paragraph, min_length=10))
-            return full
+            except Exception as e:
+                raise Exception(e)
 
+        #   BERT
+        elif self.model_name == 'BERT':
+            try:
+                bert_model = Summarizer()
+                bert_summary = ''.join(bert_model(paragraph, min_length=10))
+                return bert_summary
 
+            except Exception as e:
+                raise Exception(e)
+
+        #   XLNet
+        elif self.model_name == 'XLNet':
+            try:
+                model = TransformerSummarizer(transformer_type="XLNet", transformer_model_key="xlnet-base-cased")
+                full = ''.join(model(paragraph, min_length=10))
+                return full
+
+            except Exception as e:
+                raise Exception(e)
